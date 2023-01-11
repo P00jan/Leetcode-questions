9fde6697791e16a246cf5fa4c5af8765dfd5b581
P00jan <poojan2132000p@gmail.com>
2023-01-10 20:03:50 -0500

    Contains Duplicate

--- DS&Algo/Arrays/Array101.py
+++ DS&Algo/Arrays/Array101.py
@@ -1,10 +1,14 @@
+"""
+Contains Duplicate Problem
+"""
+
 usr_choice = int(input("Enter no of your choice: "))
 
 for i in range(usr_choice):
     array1 = list(input("Enter a array:"))
     print (array1)
     print (len(array1))
-    
+
     for i in range(array1):
         if array1.count > 1:
             print("True")
